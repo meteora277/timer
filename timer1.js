afb3ba7b8275f63e6b8b25b8cb61d773e3ff9d34
meteora277 <justin.s.diaz@gmail.com>
2021-11-29 23:54:26 -0500

    created logic for handling non number arguements

--- timer1.js
+++ timer1.js
@@ -1,25 +1,26 @@
 let args = process.argv.slice(2);
-args = args.map(item => parseInt(item));
+args = args.map((item) => parseInt(item));
 
-const timer = function(numsArray) {
+const timer = function (numsArray) {
+  if (numsArray.length === 0) {
+    return undefined;
+  }
 
-  const recursiveTimer = function(num) {
-
+  const recursiveTimer = function (num) {
     setTimeout(() => {
-
       if (num !== 0) {
         return recursiveTimer(num - 1);
       } else {
-  
-        console.log('-beep-');
+        console.log("-beep-");
       }
-
     }, 1000);
-
   };
 
-  numsArray.forEach(item => recursiveTimer(item));
-
+  numsArray.forEach((item) => {
+    if (item > 0 && typeof item === "number") {
+      recursiveTimer(item);
+    }
+  });
 };
 
 timer(args);
